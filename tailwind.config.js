509dc54656ad0e488b817ce9f63f1356bee03382
Lucas Cerri <lucascerri3@outlook.de>
2022-11-12 15:23:41 +0100

    project setup

---
+++ tailwind.config.js
@@ -0,0 +1,22 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["public/*.html", "public/js/*.js"],
+  theme: {
+    extend: {
+      fontFamily: {
+        Kumbh_Sans: ["Kumbh Sans", "sans-serif"],
+      },
+      colors:{
+        orange: "hsl(26, 100%, 55%)",
+        pale_orange: "hsl(26, 100%, 55%)",
+        very_dark_blue: "hsl(220, 13%, 13%)",
+        dark_grayish_blue: "hsl(219, 9%, 45%)",
+        grayish_blue: "hsl(220, 14%, 75%)",
+        light_grayish_blue: "hsl(223, 64%, 98%)",
+        white: "hsl(0, 0%, 100%)",
+        black: "hsl(0, 0%, 0%)", // 75% opacity needed
+      }
+    },
+  },
+  plugins: [],
+}
